fd0b5180dfd96d72dfc99485502208607e5400ce
Sam Corchero <sam.corchero@enti.cat>
2024-11-20 11:23:23 +0100

    ultima clase cambios

--- clientclass.sh
+++ clientclass.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-PORT="2022"
 
-echo "Drágon Magia Abuelita Miedo 2022"
+if [ "$1" == "" ]
+then
+	echo "Debes indicar la dirección IP del servidor."
+	echo "Ejemplo: "
+	echo -e "\t$0 127.0.0.1"
+	exit 1
+fi
+
+IP_SERVER=$1
+
+PORT="2022"
+IP=`ip a | grep "scope global" | xargs | cut -d " " -f 2 | cut -d "/" -f 1`
+
+echo "Cliente Drágon Magia Abuelita Miedo 2022"
 echo "1. ENVIO DE CABECERA"
 
-echo "DMAM" | nc 127.0.0.1 $PORT
+echo "DMAM $IP" | nc $IP_SERVER $PORT
 
 DATA=`nc -l $PORT`
 
@@ -21,7 +33,7 @@ echo "3. ENVIO DE PREFIJOS"
 
 FILE_NAME="dragon.txt"
 
-echo "FILE_NAME $FILE_NAME" | nc localhost $PORT
+echo "FILE_NAME $FILE_NAME" | nc $IP_SERVER $PORT
 
 DATA=`nc -l $PORT`
 
@@ -35,7 +47,7 @@ echo "PREFIJO enviado con éxito!"
 
 echo "5. ENVIO DE CONTENIDO"
 CONTENIDO=`cat client/$FILE_NAME`
-echo "$CONTENIDO" | nc localhost $PORT
+echo "$CONTENIDO" | nc $IP_SERVER $PORT
 
 DATA=`nc -l $PORT`
 
@@ -45,6 +57,4 @@ then
 	exit 3
 fi
 echo "CONTENIDO enviado con éxito!"
-
-
 
--- serverclass.sh
+++ serverclass.sh
@@ -3,17 +3,18 @@ PORT="2022"
 
 echo "Servidor de Dragón Magia Abuelita Miedo 2022"
 echo "0. ESCUCHANDO"
-
-DATA=`nc -l $PORT`
-
-if [ "$DATA" != "DMAM" ]
+DATA=`nc -l $PORT` 
+HEADER=`echo "$DATA" | cut -d ' ' -f 1`
+IP=`echo "$DATA" | cut -d ' ' -f 2`
+echo "IP del CLIENTE - $IP"
+if [ "$HEADER" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc localhost $PORT
+	echo "KO_HEADER" | nc $IP $PORT
 	exit 1
 fi
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc localhost $PORT
+echo "OK_HEADER" | nc $IP $PORT
 
 
 
@@ -24,11 +25,11 @@ FILE_NAME=`echo "$DATA" | cut -d ' ' -f 2`
 if [ "$PREFIJO" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_FILE_NAME" | nc localhost $PORT
+	echo "KO_FILE_NAME" | nc $IP $PORT
 	exit 2
 fi
 echo "4. CHECK OK - Enviando OK_FILE_NAME"
-echo "OK_FILE_NAME" | nc localhost $PORT
+echo "OK_FILE_NAME" | nc $IP $PORT
 
 
 
@@ -39,7 +40,7 @@ DATA=`nc -l $PORT`
 if [ "$DATA" == "" ]
 then
 	echo "ERROR 3: No hay contenido/contenido vacío"
-	echo "KO_CONTENIDO" | nc localhost $PORT
+	echo "KO_CONTENIDO" | nc $IP $PORT
 	exit 3
 fi
 
